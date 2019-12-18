7efd03bea6f0fe93ec46cd2e13b51d5849e7c4e3
Matheus Costa <matheus271200.ms@gmail.com>
2019-12-18 12:44:58 -0300

    Sudindo alteração de curriculo

--- js/main.js
+++ js/main.js
@@ -4,3 +4,14 @@ $('.carrossel').slick({
     autoplay: true,
     autoplaySpeed: 3100,
 }); 
+
+$(window).scroll(function()
+{
+	if($(this).scrollTop() > 1000)
+	{
+	    $('#grid_navbar').addClass('fixed');
+	}else
+	{
+	    $('#grid_navbar').removeClass('fixed');
+	}
+});
